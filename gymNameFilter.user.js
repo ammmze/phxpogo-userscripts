0bd3138cca53b1cee50a102be495d4d57ecfb756
Branden <ammmze@gmail.com>
2017-10-24 15:30:34 -0700

    re-write to support the pro map

--- gymNameFilter.user.js
+++ gymNameFilter.user.js
@@ -1,69 +1,69 @@
 // ==UserScript==
 // @name         PhxPogoMap - Gym Name Filter
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2.0
 // @description  try to take over the world!
 // @author       You
-// @match        https://phxpogomap.com/*/map
+// @match        https://phxpogomap.com/phx
 // @grant        none
 // ==/UserScript==
 
 (function($) {
     'use strict';
 
-    var $gymNameFilter = $('#gym-name-filter-wrapper');
-    if ($gymNameFilter.length === 0) {
-        // need to add the gym name filter
-        StoreOptions.gymNameFilter = {default: '', type: {
-            parse: function (str) {
-                return !str || !str.length ? [] : str.split(',');
-            },
-            stringify: function (opt) {
-                if (typeof opt === 'string') {
-                    return opt;
-                }
-                return opt && opt.join && opt.join(',') || opt.toString();
-            }
-        }};
-        $gymNameFilter = $('<div class="form-control switch-container" id="gym-name-filter-wrapper" />');
-        $gymNameFilter.append('<h3>Gym Names (csv)</h3>');
-        var $input = $('<input type="text" class="gym-name-filter-switch" placeholder="GET YOUR LEVEL BADGE,Starbucks" />');
-        $input.val(Store.get('gymNameFilter'));
-        $input.on('change', function(e) {
-            Store.set('gymNameFilter', e.target.value.trim());
-            lastgyms = false;
-            updateMap();
+    var GYM_NAME_FILTER = 'GYM_NAME_FILTER';
+
+    function setupFilterPanel() {
+        var filterPanel = $('[data-panel="filters"] .panel-body');
+        if (filterPanel.length === 0) {
+            console.error('could not find the filter panel');
+            return;
+        }
+
+        var gymNameFilter = $('<div />'), input;
+        gymNameFilter.append('<label for="GYM_NAME_FILTER">Gym Name Filter</label>');
+        gymNameFilter.append(input = $('<input name="GYM_NAME_FILTER" class="form-control" placeholder="(GET YOUR LEVEL BADGE|Starbucks)" type="text" />'));
+        input.val(localStorage.getItem(GYM_NAME_FILTER) || '');
+        filterPanel.prepend(gymNameFilter);
+        input.on('change', function () {
+            localStorage.setItem(GYM_NAME_FILTER, $(this).val());
+            if (!overlays.Raids.hidden) getRaids();
+            if (!overlays.Gyms.hidden) getGyms();
         });
-        $gymNameFilter.append($input);
-        $gymNameFilter.insertAfter($('#gyms-filter-wrapper'));
     }
 
-    if (!window.replacedProcessGym) {
-        window.replacedProcessGym = true;
-        var origProcessGym = window.processGym;
-        window.processGym = function (i, item) {
-            var removeGymFromMap = function removeGymFromMap(gymid) {
-                if (mapData.gyms[gymid] && mapData.gyms[gymid].marker) {
-                    if (mapData.gyms[gymid].marker.rangeCircle) {
-                        mapData.gyms[gymid].marker.rangeCircle.setMap(null);
-                    }
-                    mapData.gyms[gymid].marker.setMap(null);
-                    delete mapData.gyms[gymid];
-                }
-            };
+    var filterGymName = function (data) {
+        var gymNameFilter = localStorage.getItem(GYM_NAME_FILTER) || '';
+        if (gymNameFilter && gymNameFilter.length > 0) {
+            var filter = new RegExp(gymNameFilter, 'i');
+            console.log('filtering gyms with filter', gymNameFilter);
+            return data.filter(function (raid) {
+                return filter.test(raid.name);
+            });
+        }
+        return data;
+    };
 
-            var names = Store.get('gymNameFilter');
-            if (names && names.length) {
-                var allowed = false;
-                $.each(names, function (i, filterName) {
-                    allowed = allowed || new RegExp(filterName, 'i').test(item.name);
-                });
-                if (!allowed) {
-                    removeGymFromMap(item.gym_id);
-                    return true;
-                }
-            }
-            return origProcessGym(i, item);
-        };
+    var origAddRaidsToMap = window.addRaidsToMap;
+    var addRaidsToMap = function (data, map) {
+        return origAddRaidsToMap(filterGymName(data), map);
+    };
+
+    var origAddGymsToMap = window.addGymsToMap;
+    var addGymsToMap = function (data, map) {
+        return origAddGymsToMap(filterGymName(data), map);
+    };
+
+    // replace stuff in window
+    window._defaultSettings.GYM_NAME_FILTER = window._defaultSettings.GYM_NAME_FILTER || '';
+    if (window.addRaidsToMap !== addRaidsToMap) {
+        window.addRaidsToMap = addRaidsToMap;
     }
+
+    if (window.addGymsToMap !== addGymsToMap) {
+        window.addGymsToMap = addGymsToMap;
+    }
+
+    // setup filter panel
+    setupFilterPanel();
 })(window.jQuery);
